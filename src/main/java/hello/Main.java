2cc01030c74c2c90b5aec7478d81e66302c4921d
Антон <tohak199@gmail.com>
2018-11-25 13:25:07 +0200

    changed the logic

--- src/main/java/hello/Main.java
+++ src/main/java/hello/Main.java
@@ -8,6 +8,6 @@ import java.util.Locale;
 public class Main {
     public static void main(String[] args) {
         MessageService helloWorld = new MessageService();
-        System.out.printf(helloWorld.getMessage(LocalTime.now(), Locale.getDefault()));
+        System.out.println(helloWorld.getMessage(LocalTime.now(), Locale.getDefault()));
     }
 }
--- src/main/java/hello/service/MessageService.java
+++ src/main/java/hello/service/MessageService.java
@@ -8,22 +8,19 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class MessageService {
-    private static final LocalTime SIX_HOURS = LocalTime.of(6, 0);
-    private static final LocalTime NINE_HOURS = LocalTime.of(9, 0);
-    private static final LocalTime NINETEEN_HOURS = LocalTime.of(19, 0);
-    private static final LocalTime TWENTY_THREE_HOURS = LocalTime.of(23, 0);
     private static final Logger log = Logger.getLogger(MessageService.class);
 
     public String getMessage(LocalTime timeNow, Locale locale) {
         ResourceBundle resourceBundle = ResourceBundle.getBundle("messageResource", locale);
         String message;
-        if (timeNow.isAfter(SIX_HOURS) && timeNow.isBefore(NINE_HOURS)) {
+        int hour = timeNow.getHour();
+        if (6 <= hour && hour < 9) {
             log.info("Added message in the morning");
             message = resourceBundle.getString("morning");
-        } else if (timeNow.isAfter(NINE_HOURS) && timeNow.isBefore(NINETEEN_HOURS)) {
+        } else if (9 <= hour && hour < 19) {
             log.info("Added message in the day");
             message = resourceBundle.getString("day");
-        } else if (timeNow.isAfter(NINETEEN_HOURS) && timeNow.isBefore(TWENTY_THREE_HOURS)) {
+        } else if (19 <= hour && hour < 23) {
             log.info("Added message in the evening");
             message = resourceBundle.getString("evening");
         } else {
